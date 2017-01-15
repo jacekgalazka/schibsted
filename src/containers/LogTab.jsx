1d4671b55ab16fca6a4d9e31c6707e16cb7b5eaa
Jacek Gałązka <jacek.galazka@atos.net>
2017-01-15 20:02:54 +0100

    log tab - data sorting

--- src/containers/LogTab.jsx
+++ src/containers/LogTab.jsx
@@ -7,10 +7,42 @@ export class LogTab extends Component {
     this.props.fetchLog();
   }
 
+  getSplitedArray() {
+    let objectArray = this.props.fileData ? this.props.fileData.split(/\n/) : [];
+    objectArray = objectArray.map((item) => {
+      const elementArray = item.split(' ');
+      return [elementArray[0], elementArray[6], item];
+    });
+    return objectArray;
+  }
+
+  groupElements(arr) {
+    const hostGroups = {};
+    const fileGroups = {};
+    arr.forEach((item) => {
+      hostGroups[item[0]] ? hostGroups[item[0]].push(item) : hostGroups[item[0]] = [item];
+      fileGroups[item[1]] ? fileGroups[item[1]].push(item) : fileGroups[item[1]] = [item];
+    });
+
+    return { hostGroups, fileGroups };
+  }
+
+  renderHostList(sortedObjects) {
+    console.log(sortedObjects.hostGroups);
+  }
+
+  renderFileList(sortedObjects) {
+    console.log(sortedObjects.fileGroups);
+  }
+
   renderLogObjectList() {
+    const splitedLogArray = this.getSplitedArray();
+    const sortedObjects = splitedLogArray.length ? this.groupElements(splitedLogArray, 0) : {};
+
     return (
       <div>
-        loooog
+        {this.renderHostList(sortedObjects)}
+        {this.renderFileList(sortedObjects)}
       </div>
     );
   }
